98e34067bd1c0759a3ec5459d50cd70f8b20ac3c
msnaru21 <93150586+msnaru21@users.noreply.github.com>
2023-02-16 13:49:52 +0530

    Add files via upload

---
+++ PES1UG20CS664.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Naresh Srinivas\n";
+  cout << "Hello, PES1UG20CS664\n";
+  cout << "Hello, Cloud Computing\n";
+  return 0;
+}
